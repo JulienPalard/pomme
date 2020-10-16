ccc4ad121907e707f15e61b62976fd179c2378e1
Jules Lasne (jlasne - seluj78) <jules.lasne@gmail.com>
2020-10-16 16:49:59 +0200

    Added base for frontend

--- pomme/routes/views/home.py
+++ pomme/routes/views/home.py
@@ -1,8 +1,9 @@
 from flask import Blueprint
+from flask import render_template
 
 home_bp = Blueprint("home", __name__)
 
 
 @home_bp.route("/", methods=["GET"])
 def home_view():
-    return "HI"
+    return render_template("metrics.html")
