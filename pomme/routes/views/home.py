3752a23b86486a14d885094a56faa892a06a5b6d
Jules Lasne (jlasne - seluj78) <jules.lasne@gmail.com>
2020-10-16 18:13:40 +0200

    Split file into jinja blocks

--- pomme/routes/views/home.py
+++ pomme/routes/views/home.py
@@ -6,4 +6,19 @@ home_bp = Blueprint("home", __name__)
 
 @home_bp.route("/", methods=["GET"])
 def home_view():
+    return render_template("home.html")
+
+
+@home_bp.route("/metrics", methods=["GET"])
+def metrics_view():
     return render_template("metrics.html")
+
+
+@home_bp.route("/search", methods=["GET"])
+def search_view():
+    return render_template("search.html")
+
+
+@home_bp.route("/crawl", methods=["GET"])
+def crawl_view():
+    return render_template("crawl.html")
